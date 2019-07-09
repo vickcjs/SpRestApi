79c1b23d4070cddb9c8872449e960171c82f1651
Vick Chi <vick.chi@ncs.com.sg>
2019-07-09 16:02:59 +0800

    Initial commit

--- src/main/java/com/spapi/SpRestApiApplication.java
+++ src/main/java/com/spapi/SpRestApiApplication.java
@@ -2,13 +2,20 @@ package com.spapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = { "com.spapi" })
 @EnableJpaRepositories("com.spapi")
-public class SpRestApiApplication {
+public class SpRestApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpRestApiApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpRestApiApplication.class);
+    }
 }
